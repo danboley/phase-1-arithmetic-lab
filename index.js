d170219b14ccba84932907fa6fefe2948f0b17d0
Daniel Boley <danboley1@gmail.com>
2022-08-28 20:50:08 -0400

    commit tree 985effb5c47bf0751800c22fa4c640e2bef56284

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 4;
+let num4 = 20;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
